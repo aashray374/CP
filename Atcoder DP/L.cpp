1d21d213e681d9b72d5da2c193ab3696c68baf7a
aashray374 <aashraymahajan374@gmail.com>
2026-06-02 18:04:45 +0530

    Deque - optimized

--- Atcoder DP/L.cpp
+++ Atcoder DP/L.cpp
@@ -13,45 +13,85 @@ const int M = 1e9+7;
 
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
 
-pair<int,int> solve(int i,int j,vector<int> &v,vector<vector<pair<int,int>>> &dp){
-    int used = v.size()-(j-i+1);
-    int turn = used%2;
-    if(i == j){
-        if(turn == 0){
-            return {v[i],0};
-        }else{
-            return {0,v[i]};
-        }
-    }
+// int solve(int i,int j,vector<int> &v,vector<vector<int>> &dp){
+//     int used = v.size()-(j-i+1);
+//     int turn = used%2;
+//     if(i == j){
+//         if(turn == 0){
+//             return v[i];
+//         }else{
+//             return (-1*v[i]);
+//         }
+//     }
 
-    if(dp[i][j].first != -1) return dp[i][j];
+//     if(dp[i][j] != INT_MIN) return dp[i][j];
 
-    pair<int,int> op1 = solve(i+1,j,v,dp),op2 = solve(i,j-1,v,dp),ans;
+//     int op1 = solve(i+1,j,v,dp),op2 = solve(i,j-1,v,dp),ans;
 
-    if(turn == 0){
-        int a1 = v[i]+op1.first-op1.second,a2 = v[j]+op2.first-op2.second;
+//     if(turn == 0){
+//         int a1 = v[i]+op1,a2 = v[j]+op2;
 
-        if(a1>a2){
-            op1.first+=v[i];
-            ans = op1;
-        }else{
-            op2.first+=v[j];
-            ans = op2;
-        }
-    }else{
-        int a1 = op1.first-op1.second-v[i],a2 = op2.first-op2.second-v[j];
+//         if(a1>a2){
+//             op1+=v[i];
+//             ans = op1;
+//         }else{
+//             op2+=v[j];
+//             ans = op2;
+//         }
+//     }else{
+//         int a1 = op1-v[i],a2 = op2-v[j];
 
-        if(a1<a2){
-            op1.second+=v[i];
-            ans = op1;
-        }else{
-            op2.second+=v[j];
-            ans = op2;
-        }
-    }
+//         if(a1<a2){
+//             op1-=v[i];
+//             ans = op1;
+//         }else{
+//             op2-=v[j];
+//             ans = op2;
+//         }
+//     }
 
-    return dp[i][j] = ans;
-}
+//     return dp[i][j] = ans;
+// }
+
+// pair<int,int> solve(int i,int j,vector<int> &v,vector<vector<pair<int,int>>> &dp){
+//     int used = v.size()-(j-i+1);
+//     int turn = used%2;
+//     if(i == j){
+//         if(turn == 0){
+//             return {v[i],0};
+//         }else{
+//             return {0,v[i]};
+//         }
+//     }
+
+//     if(dp[i][j].first != -1) return dp[i][j];
+
+//     pair<int,int> op1 = solve(i+1,j,v,dp),op2 = solve(i,j-1,v,dp),ans;
+
+//     if(turn == 0){
+//         int a1 = v[i]+op1.first-op1.second,a2 = v[j]+op2.first-op2.second;
+
+//         if(a1>a2){
+//             op1.first+=v[i];
+//             ans = op1;
+//         }else{
+//             op2.first+=v[j];
+//             ans = op2;
+//         }
+//     }else{
+//         int a1 = op1.first-op1.second-v[i],a2 = op2.first-op2.second-v[j];
+
+//         if(a1<a2){
+//             op1.second+=v[i];
+//             ans = op1;
+//         }else{
+//             op2.second+=v[j];
+//             ans = op2;
+//         }
+//     }
+
+//     return dp[i][j] = ans;
+// }
 
 void testcase() {
     int n;cin>>n;
@@ -62,13 +102,37 @@ void testcase() {
         cin>>v[i];
     }
 
-    vector<vector<pair<int,int>>> dp(n,vector<pair<int,int>>(n,{-1,-1}));
-
-    pair<int,int> ans = solve(0,n-1,v,dp);
-
-    cout<<ans.first-ans.second<<endl;
-
+    vector<int> dp(n);
     
+    // base case length = 1
+    for(int i = 0;i<n;i++){
+        int turn = n%2;
+        if(turn == 0){
+            dp[i] = -1*v[i];
+        }else{
+            dp[i] = v[i];
+        }
+    }
+
+
+    for(int len = 2;len<=n;len++){
+        vector<int> curr(n,0);
+        
+        int used = n-len;
+        int turn = used%2;
+        for(int i = 0;i<n-len+1;i++){
+            int j = i+len-1;
+            if(turn == 0){
+                int op1 = dp[i]+v[j],op2 = dp[i+1]+v[i];
+                dp[i] = max(op1,op2);
+            }else{
+                int op1 = dp[i]-v[j],op2 = dp[i+1]-v[i];
+                dp[i] = min(op1,op2);
+            }
+        }
+    }
+
+    cout<<dp[0]<<endl;
 }
 
 
