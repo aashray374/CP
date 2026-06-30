0c5ebc3126e08a739dee5879b09da479b2f1a919
aashray374 <aashraymahajan374@gmail.com>
2026-06-30 12:30:40 +0530

    counting paths

---
+++ cses/tree/countingPaths.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define alice cout<<"Alice"<<endl;
+#define bob cout<<"Bob"<<endl;
+#define impo cout<<"IMPOSSIBLE"<<endl;
+#define log logl
+const int M = 1e9+7;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+
+void precompute(){
+
+}
+
+void getDepth(vector<vector<int>> &tree,vector<int> &depth,int prev,int curr,int parent,vector<int> &p){
+    p[curr] = parent;
+    for(int &x : tree[curr]){
+        if(x != parent){
+            depth[x] = prev+1;
+            getDepth(tree,depth,depth[x],x,curr,p);
+        }
+    }
+}
+void testcase() {
+    
+    int n,q;cin>>n>>q;
+
+    vector<vector<int>> ancs,tree(n);
+    vector<int> parent(n,-1),depth(n,0);
+
+    for(int i = 1;i<n;i++){
+        // int t;cin>>t;t--;
+        // parent[i] = t;
+        int u,v;cin>>u>>v;
+        u--;v--;
+        tree[u].push_back(v);
+        tree[v].push_back(u);
+    }
+    getDepth(tree,depth,0,0,-1,parent);
+    int curr = 2;
+    vector<int> prev = parent;
+    ancs.push_back(parent);
+    while(curr<n){
+        vector<int> temp(n,-1);
+        for(int i = 0;i<n;i++){
+            if(prev[i] != -1){
+                temp[i] = prev[prev[i]];
+            }
+        }
+        ancs.push_back(temp);
+        prev = temp;
+        curr<<=1;
+    }
+    vector<int> add(n,0),sub(n,0);
+
+    while(q--){
+        int u,v;cin>>u>>v;
+        // cout<<u<<" "<<v<<endl;
+        u--;
+        v--;
+        add[u]++;
+        add[v]++;
+        if(depth[u]<depth[v]){
+            swap(v,u);
+        }
+
+        int k = depth[u]-depth[v];
+        for(int i = 0;i<ancs.size() && k!= 0 && u != -1;i++){
+            if(((1<<i)&k)){
+                u = ancs[i][u];
+                k-=(1<<i);
+            }
+        }
+        int lca = u;
+        if(u != v){
+            for(int i = ancs.size()-1;i>=0;i--){
+                if(ancs[i][u] != ancs[i][v]){
+                    u = ancs[i][u];
+                    v = ancs[i][v];
+                }
+            }
+            lca = parent[u];
+        }
+        // cout<<lca<<endl;
+        sub[lca]+=2;
+    }
+    function<void(int, int)> dfs = [&](int curr,int parent){
+        
+        for(int &x : tree[curr]){
+            if(x != parent){
+                dfs(x,curr);
+                add[curr]+=add[x]-sub[x]/2;
+            }
+        }
+        add[curr] = add[curr]-sub[curr]/2;
+    };
+    
+    dfs(0,-1);
+    for(int i = 0;i<n;i++){
+        cout<<add[i]<<" ";
+    }cout<<endl;
+
+}
+
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+    int t = 1;
+    // cin>>t;
+    precompute();
+    while(t--){
+        testcase();
+    }
+
+}
